1f4b71bc33bb5e0518992f944cfaa2e1227c749d
Nguyễn Thị Trà My <89174992+ashleynguyen07@users.noreply.github.com>
2023-10-25 03:33:04 +0700

    Create Staff.java

---
+++ src/main/java/Model/Staff.java
@@ -0,0 +1,73 @@
+package Model;
+
+/**
+ *
+ * @author tramy
+ */
+public class Staff {
+    private String staffid;
+    private String lastname;
+    private String firstname;
+    private String email;
+    private int phone;
+
+    public Staff() {
+    }
+
+
+    // select staff by staffid
+    public Staff(String staffid, String lastname, String firstname, String email, int phone) {
+        this.staffid = staffid;
+        this.lastname = lastname;
+        this.firstname = firstname;
+        this.email = email;
+        this.phone = phone;
+    }    
+
+    public String getStaffid() {
+        return staffid;
+    }
+
+    public void setStaffid(String staffid) {
+        this.staffid = staffid;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return "Staff{" + "staffid=" + staffid + ", lastname=" + lastname + ", firstname=" + firstname + ", email=" + email + ", phone=" + phone + '}';
+    }
+
+
+}
